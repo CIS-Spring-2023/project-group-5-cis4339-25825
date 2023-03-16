fd6ef0acc73e2742e0b811984f4c33ca2dcf6c26
HannHD <89676786+HannHD@users.noreply.github.com>
2023-03-16 18:29:38 -0500

    Add files via upload

--- frontend/src/views/login.vue
+++ frontend/src/views/login.vue
@@ -1,21 +1,32 @@
 <template>
-    <div class="row justify-content-center">
+  <main>
+    <form>
+      <div class="row justify-content-center">
           <div class="col-md-4">
-              <h3 class="text-center">Login</h3>
+              <h1 class="font-bold text-4xl text-red-700 tracking-widest text-center mt-10">
+                Login</h1>
               <form @submit.prevent="store.login(username, password)" novalidate="true">
                   <div class="form-group">
                       <label>User name</label>
-                      <input type="text" class="form-control" v-model="username" placeholder="username" required>
+                      <input type="text" class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                       v-model="username" placeholder="username" required>
                   </div>
                   <br>
                   <div class="form-group">
                       <label>Password</label>
-                      <input type="password" class="form-control" v-model="password" placeholder="password" required>
+                      <input type="password" class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                       v-model="password" placeholder="password" required>
                   </div>
-                  <button class="bg-red-700 text-white rounded">Submit</button>
+
+                <button class="bg-red-700 text-white rounded" type="submit">
+                  Login
+                </button>
+          
               </form>
           </div>
-    </div>
+        </div>
+      </form>
+    </main>
   </template>
   
   <script>
