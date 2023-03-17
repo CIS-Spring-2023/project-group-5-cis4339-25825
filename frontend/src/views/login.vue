bb48389442c0054c4a1248e3aaa16ac09b5262ba
HannHD <89676786+HannHD@users.noreply.github.com>
2023-03-16 21:16:48 -0500

    Add files via upload

--- frontend/src/views/login.vue
+++ frontend/src/views/login.vue
@@ -3,26 +3,27 @@
     <form>
       <div class="row justify-content-center">
           <div class="col-md-4">
-              <h1 class="font-bold text-4xl text-red-700 tracking-widest text-center mt-10">
+              <h1 class="font-bold text-4xl text-red-700 text-center mt-10">
                 Login</h1>
+              <div class="mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10">
               <form @submit.prevent="store.login(username, password)" novalidate="true">
                   <div class="form-group">
                       <label>User name</label>
-                      <input type="text" class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                      <input type="text" class="rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                        v-model="username" placeholder="username" required>
                   </div>
                   <br>
                   <div class="form-group">
                       <label>Password</label>
-                      <input type="password" class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                      <input type="password" class="rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                        v-model="password" placeholder="password" required>
                   </div>
-
                 <button class="bg-red-700 text-white rounded" type="submit">
                   Login
                 </button>
           
               </form>
+            </div>
           </div>
         </div>
       </form>
