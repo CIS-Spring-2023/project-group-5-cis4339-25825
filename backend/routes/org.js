af3c127aaef469b19a132a07c164cc1cc461e79d
qle8 <70501212+qle8@users.noreply.github.com>
2023-05-01 23:02:11 -0500

    Add files via upload

--- backend/routes/org.js
+++ backend/routes/org.js
@@ -1,4 +1,3 @@
-/* eslint-disable prettier/prettier */
 const express = require('express')
 const router = express.Router()
 
@@ -7,15 +6,14 @@ const org = process.env.ORG
 // importing data model schemas
 const { orgs } = require('../models/models')
 
-// GET org
-router.get('/', (req, res, next) => {
-  orgs.findById(org, (error, data) => {
-    if (error) {
-      return next(error)
-    } else {
-      res.json(data)
-    }
-  })
-})
+router.get("/", async (request, response) => {
+  const users = await orgs.find({});
+
+  try {
+    response.send(users);
+  } catch (error) {
+    response.status(500).send(error);
+  }
+});
 
 module.exports = router
--- backend/routes/services.js
+++ backend/routes/services.js
@@ -1,93 +1,112 @@
-/* eslint-disable prettier/prettier */
 const express = require('express')
 const router = express.Router()
 
 const org = process.env.ORG
 
 // importing data model schemas
-const { orgs } = require('../models/models')
+const { services } = require('../models/models')
 /////////// APIs for 'Add Service' page /////////////////
 //All CRUD ops for services
 
-// GET service by ID
-router.get('/getservice/:id', async (req, res) => {
-    try {
-      const service = await service.findById(req.params.id)
-      res.json(service)
-    } catch (err) {
-      res.status(500).json({ message: err.message })
-    }
-  })
-
-// POST create new service
-router.post('/createservice/', async (req, res) => {
-    const service = new service({
-      name: req.body.name,
-      description: req.body.description,
-      status: req.body.status
-    })
-    try {
-      const newService = await service.save()
-      res.status(201).json(newService)
-    } catch (err) {
-      res.status(400).json({ message: err.message })
-    }
-  })
-
-// PUT update existing service
-router.put('/updateservice/:id', async (req, res) => {
-    try {
-      const service = await service.findById(req.params.id)
-      if (req.body.name) {
-        service.name = req.body.name
-      }
-      if (req.body.description) {
-        service.description = req.body.description
-      }
-      if (req.body.status) {
-        service.status = req.body.status
-      }
-      const updatedService = await service.save()
-      res.json(updatedService)
-    } catch (err) {
-      res.status(400).json({ message: err.message })
-    }
-  })
-
-// DELETE existing service
-router.delete('/deleteservice/:id', async (req, res) => {
-    try {
-      const service = await service.findById(req.params.id)
-      await service.remove()
-      res.json({ message: 'Service has been deleted' })
-    } catch (err) {
-      res.status(500).json({ message: err.message })
-    }
-  })
-
-////////// APIs for 'find Service' page /////////////////
-// GET services based on search query
-router.get('/search/', (req, res, next) => {
-    const dbQuery = { org: org }
-    switch (req.query.searchBy) {
-      case 'name':
-        // match event name, no anchor
-        dbQuery.name = { $regex: `${req.query.name}`, $options: 'i' }
-        break
-      case 'date':
-        dbQuery.date = req.query.eventDate
-        break
-      default:
-        return res.status(400).send('invalid searchBy')
-    }
-    events.find(dbQuery, (error, data) => {
+// GET 10 most recent events for org
+router.get('/', (req, res, next) => {
+  services
+    .find({ org: org }, (error, data) => {
       if (error) {
         return next(error)
       } else {
-        res.json(data)
+        return res.json(data)
       }
     })
-  })
+    // sort by date ascending
+    .sort({ date: 1 })
+    .limit(10)
+})
 
-//
+// GET service by ID
+router.get('/id/:id', (req, res, next) => {
+  // use findOne instead of find to not return array
+  services.findOne({ _id: req.params.id }, (error, data) => {
+    if (error) {
+      return next(error)
+    } else if (!data) {
+      res.status(400).send('Service not found')
+    } else {
+      res.json(data)
+    }
+  })
+})
+
+// GET services based on service query
+router.get('/search/', (req, res, next) => {
+  const dbQuery = { org: org }
+  switch (req.query.searchBy) {
+    case 'name':
+      // match event name, no anchor
+      dbQuery.name = { $regex: `${req.query.name}`, $options: 'i' }
+      break
+    case 'description':
+      dbQuery.description = {
+        $regex: `${req.query.description}`,
+        $options: 'i'
+      }
+      break
+    default:
+      return res.status(400).send('invalid searchBy')
+  }
+  events.find(dbQuery, (error, data) => {
+    if (error) {
+      return next(error)
+    } else {
+      res.json(data)
+    }
+  })
+})
+
+// POST new event
+router.post('/', (req, res, next) => {
+  const newService = req.body
+  newService.org = org
+  services.create(newService, (error, data) => {
+    if (error) {
+      return next(error)
+    } else {
+      res.json(data)
+    }
+  })
+})
+
+// PUT update event
+router.put('/update/:id', (req, res, next) => {
+  services.findByIdAndUpdate(req.params.id, req.body, (error, data) => {
+    if (error) {
+      return next(error)
+    } else {
+      res.json(data)
+    }
+  })
+})
+
+// hard DELETE event by ID, as per project specifications
+router.delete('/:id', (req, res, next) => {
+  services.findByIdAndDelete(req.params.id, (error, data) => {
+    if (error) {
+      return next(error)
+    } else if (!data) {
+      res.status(400).send('Service not found')
+    } else {
+      res.send('Service deleted')
+    }
+  })
+})
+
+// POST create new service
+//router.post('/createservice/', async (req, res) => {
+
+// PUT update existing service
+//router.put('/updateservice/:id', async (req, res) => {
+
+// DELETE existing service
+//router.delete('/deleteservice/:id', async (req, res) => {
+
 module.exports = router
